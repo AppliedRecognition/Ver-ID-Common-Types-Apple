1707b95f69e91a9d81065261ea954b10085bfbff
Jakub Dolejs <jakubdolejs@gmail.com>
2025-06-06 15:32:59 +0100

    Augmented SpoofDetection protocol

--- Sources/VerIDCommonTypes/SpoofDetection.swift
+++ Sources/VerIDCommonTypes/SpoofDetection.swift
@@ -9,5 +9,15 @@ import Foundation
 
 public protocol SpoofDetection {
     
+    var confidenceThreshold: Float { get set }
+    
     func detectSpoofInImage(_ image: Image, regionOfInterest: CGRect?) async throws -> Float
+}
+
+public extension SpoofDetection {
+    
+    func isSpoofInImage(_ image: Image, regionOfInterest: CGRect?) async throws -> Bool {
+        let score = try await self.detectSpoofInImage(image, regionOfInterest: regionOfInterest)
+        return score > self.confidenceThreshold
+    }
 }
